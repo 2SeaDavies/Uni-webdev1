f470e22101eeb0141ff2302bc00e4dce3942c4bb
Craig <crdavies@hotmail.co.uk>
2016-10-03 15:57:49 +0100

    test

--- forloop.php
+++ forloop.php
@@ -1,3 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>NEW TITLE</title>
+</head>
+<body>
 <?php
 /**
  * Created by PhpStorm.
@@ -5,3 +12,28 @@
  * Date: 03/10/2016
  * Time: 15:46
  */
+
+
+for ($i = 1; $i<30; $i++)
+{
+    $printlist = array();
+    if ($i % 2 == 0) {
+        $printlist[] = "Specs";
+
+    }
+    if ($i % 3 == 0 ) {
+        $printlist[] ="Mugs";    }
+    if ($i % 4 ==0) {
+        $printlist[] ="Sausage Rolls";
+    }
+    else {
+        $printlist{} ="no products";
+    }
+    foreach ($printlist as $x) {
+        print "<p> $x</p>";
+    }
+    print "<p> are available </p>";
+}
+?>
+</body>
+</html>
