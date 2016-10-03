4923ed1bdb97b86df09a218c384d661d31a499a6
Craig Davies <crdavies@hotmail.co.uk>
2016-10-03 19:36:49 +0100

    while update

--- While.php
+++ While.php
@@ -25,6 +25,19 @@ while ($count < 30) {
         $mugs -=1;
 
     }
+    elseif (($num ==1) && ($specs >0)){
+        print "<p>specs are available</p>";
+        $specs -=1;
+
+    }
+    elseif (($num ==2) && ($sausage >0)){
+        print "<p>sausages are available</p>";
+        $sausage -=1;
+
+    }
+    else {
+        print "<p> nothing is available </p>";
+    }
     $count++;
 
 }
