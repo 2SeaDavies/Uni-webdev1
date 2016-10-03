5d5014a2830177bb5bd3c633ed700c4eb0738d22
Craig <crdavies@hotmail.co.uk>
2016-10-03 15:41:33 +0100

    test

--- array.php
+++ array.php
@@ -5,3 +5,17 @@
  * Date: 03/10/2016
  * Time: 15:37
  */
+$provisioned_activities = array("specs","recreational pharmaceuticals","Led Zeppelin");
+
+foreach ($provisioned_activities as $x) {
+    print "<p>$x</p>p>";
+}
+
+$provisioned_activities[1] ="hugs";
+foreach ($provisioned_activities as $x) {
+    print "<p>$x</p>p>";
+}
+unset($provisioned_activities[2]);
+foreach ($provisioned_activities as $x) {
+    print "<p>$x</p>p>";
+}
