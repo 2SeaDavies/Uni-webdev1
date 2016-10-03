1978dce28c1e62a370c44180f1919d4458d558bc
Craig <crdavies@hotmail.co.uk>
2016-10-03 15:30:56 +0100

    test

---
+++ switch.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 0702555
+ * Date: 03/10/2016
+ * Time: 15:28
+ */
+
+$wantedgood = "mugs";
+
+
+switch($wantedgood) {
+    case "mugs":
+        echo "you must be 18";
+        break;
+    case "specs";
+        echo "you gotta be 16 m8";
+        break;
+}
