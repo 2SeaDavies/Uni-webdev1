01f49feb9b66655d15b16047876b0d24a31791ab
Craig <crdavies@hotmail.co.uk>
2016-10-03 15:26:28 +0100

    test

---
+++ ifelse.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 0702555
+ * Date: 03/10/2016
+ * Time: 15:23
+ */
+
+$age = 27;
+if ($age > 21) {
+    echo "you can buy sausage rolls";
+}
+    elseif ($age > 18) {
+        echo "you can buy mugs";
+
+    }
+elseif ($age > 16)
+{
+    echo "you can buy glasses";
+}
---
+++ index.php
@@ -0,0 +1,29 @@
+
+/**
+ * Created by PhpStorm.
+ * User: 0702555
+ * Date: 03/10/2016
+ * Time: 15:17
+ */
+
+
+
+
+
+<!DOCTYPE html>
+<html>
+ <head>
+</head>
+<body>
+ <p>
+     <?php
+ echo "Hello World";
+
+     
+
+ ?>
+
+     <a href="ifelse.php">Week 2 lab 1 activity if/else </a>
+</p>
+</body>
+</html>
