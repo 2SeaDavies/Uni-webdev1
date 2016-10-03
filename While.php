730cc3395c5e8aedb20d10804c9707d2ec481006
Craig <crdavies@hotmail.co.uk>
2016-10-03 16:15:51 +0100

    test

---
+++ While.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>NEW TITLE</title>
+</head>
+<body>
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 0702555
+ * Date: 03/10/2016
+ * Time: 16:07
+ */
+$mugs = 7;
+$specs = 7;
+$sausage = 7;
+$count = 0;
+while ($count < 30) {
+    $num = rand(0,2);
+    print "<p> on day $count </p>";
+    if (($num ==0) && ($mugs >0)){
+        print "<p>mugs are available</p>";
+    }
+
+}
+
+
+?>
+
+</body>
+</html>
